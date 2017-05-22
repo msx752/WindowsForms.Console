fd783a94ccfd0fb8cb6e161b3c692577e28e769f
msx752 <aslim@outlook.com.tr>
2017-05-22 22:13:14 +0300

    notify upt

--- WindowsForm.Console/WindowsForm.Console/Properties/AssemblyInfo.cs
+++ WindowsForm.Console/WindowsForm.Console/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following set of attributes.
 // Change these attribute values to modify the information associated with an assembly.
 [assembly: AssemblyTitle("WindowsForm.Console")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Component of WindowsForm Console")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mustafa Salih ASLIM")]
 [assembly: AssemblyProduct("WindowsForm.Console")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
