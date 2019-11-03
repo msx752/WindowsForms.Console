4b17418e64f9ee4588611255cbf3a8bc3adb715a
msx752 <salih@aslim.net>
2019-11-03 21:07:38 +0300

    nuget package file

--- WindowsFormBuild.ps1
+++ WindowsFormBuild.ps1
@@ -6,14 +6,14 @@ if($($env:APPVEYOR)){
     $msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 }
 
-.\nuget restore WindowsForm.Console.sln;
+.\nuget restore WindowsForms.Console.sln;
 
-& $msbuild WindowsForm.Console.sln /property:Configuration=Release /p:TargetFrameworkVersion=v4.5 /p:OutputPath="bin\v4.5";
-& $msbuild WindowsForm.Console.sln /property:Configuration=Release /p:TargetFrameworkVersion=v4.5.1 /p:OutputPath="bin\v4.5.1";
-& $msbuild WindowsForm.Console.sln /property:Configuration=Release /p:TargetFrameworkVersion=v4.5.2 /p:OutputPath="bin\v4.5.2";
-& $msbuild WindowsForm.Console.sln /property:Configuration=Release /p:TargetFrameworkVersion=v4.6 /p:OutputPath="bin\v4.6";
-& $msbuild WindowsForm.Console.sln /property:Configuration=Release /p:TargetFrameworkVersion=v4.6.1 /p:OutputPath="bin\v4.6.1";
-& $msbuild WindowsForm.Console.sln /property:Configuration=Release /p:TargetFrameworkVersion=v4.6.2 /p:OutputPath="bin\v4.6.2";
-#& $msbuild WindowsForm.Console.sln /property:Configuration=Release /p:TargetFrameworkVersion=v4.7 /p:OutputPath="bin\v4.7";
+& $msbuild WindowsForms.Console.sln /property:Configuration=Release /p:TargetFrameworkVersion=v4.5 /p:OutputPath="bin\v4.5";
+& $msbuild WindowsForms.Console.sln /property:Configuration=Release /p:TargetFrameworkVersion=v4.5.1 /p:OutputPath="bin\v4.5.1";
+& $msbuild WindowsForms.Console.sln /property:Configuration=Release /p:TargetFrameworkVersion=v4.5.2 /p:OutputPath="bin\v4.5.2";
+& $msbuild WindowsForms.Console.sln /property:Configuration=Release /p:TargetFrameworkVersion=v4.6 /p:OutputPath="bin\v4.6";
+& $msbuild WindowsForms.Console.sln /property:Configuration=Release /p:TargetFrameworkVersion=v4.6.1 /p:OutputPath="bin\v4.6.1";
+& $msbuild WindowsForms.Console.sln /property:Configuration=Release /p:TargetFrameworkVersion=v4.6.2 /p:OutputPath="bin\v4.6.2";
+#& $msbuild WindowsForms.Console.sln /property:Configuration=Release /p:TargetFrameworkVersion=v4.7 /p:OutputPath="bin\v4.7";
 
 .\nuget pack WindowsForm.Console.nuspec;
